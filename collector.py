68b56bfd09f3adcb62fc4e2d318b27aab3a5f267
TanmayKadam11 <musicofmusic46@gmail.com>
2026-06-19 23:07:54 +0530

    Update collector.py

--- collector.py
+++ collector.py
@@ -1,12 +1,9 @@
 import requests
 
-url = "https://draw.ar-lottery01.com/WinGo/WinGo_1M/GetHistoryIssuePage.json?pageNo=1&pageSize=10"
+r = requests.get(
+    "https://draw.ar-lottery01.com/WinGo/WinGo_1M.json",
+    timeout=20
+)
 
-try:
-    r = requests.get(url, timeout=20)
-
-    print("Status:", r.status_code)
-    print(r.text[:500])
-
-except Exception as e:
-    print("Error:", e)
+print(r.status_code)
+print(r.text)
