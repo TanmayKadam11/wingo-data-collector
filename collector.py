7892eb3e0c02e7b0e7643bc7d444984c0a724ed3
TanmayKadam11 <musicofmusic46@gmail.com>
2026-06-19 18:52:33 +0530

    Update collector.py

--- collector.py
+++ collector.py
@@ -1,1 +1,13 @@
-print("Hello")
+import requests
+
+url = "https://draw.ar-lottery01.com/WinGo/WinGo_1M/GetHistoryIssuePage.json?pageNo=1&pageSize=10"
+
+try:
+    r = requests.get(url, timeout=20)
+
+    print("Status:", r.status_code)
+
+    print(r.text[:500])
+
+except Exception as e:
+    print("Error:", e)
